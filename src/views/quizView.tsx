683dd6593753f55ac7f91b954e46993536d02d30
zubair-y <muhammad.yousafzai@hotmail.com>
2022-12-06 22:44:50 +0100

    setting up redux

--- src/views/quizView.tsx
+++ src/views/quizView.tsx
@@ -1,9 +1,13 @@
 import '../css/quiz.css';
 
-function QuizView(){
+interface QuizViewTypes{
+    countNumber: number
+}
+
+export default function QuizView({countNumber}: QuizViewTypes){
     return(
         <div className="quizDiv">
-            <h1 className="question">Question:</h1>
+            <h1 className="question">Question: {countNumber}</h1>
             <button className="answer1">Svar 1</button>
             <button className="answer2">Svar 2</button>
             <button className="answer3">Svar 3</button>
@@ -11,4 +15,3 @@ function QuizView(){
         </div>
     )
 }
-export default QuizView;
