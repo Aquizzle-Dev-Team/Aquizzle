0f6c47196d5e22b72fd10269b38a990f5eff4b0d
Geanja <andre.eriksson2@gmail.com>
2022-12-13 20:57:08 +0100

    Fixed so that all buttons can disappear if it does not contain any answer

--- src/views/quizView.tsx
+++ src/views/quizView.tsx
@@ -8,8 +8,8 @@ export default function QuizView(props: any){
     return(
             <div className="quizDiv">
                 <h1 className="question">{props.question}</h1>
-                <button className="answer1" onClick = {props.onClickAnswer}>{props.answer1}</button>
-                <button className="answer2" onClick = {props.onClickAnswer}>{props.answer2}</button>
+                <button className={props.answer3? "answer1" : "hideButton"} onClick = {props.onClickAnswer}>{props.answer1}</button>
+                <button className={props.answer3? "answer2" : "hideButton"} onClick = {props.onClickAnswer}>{props.answer2}</button>
                 <button className={props.answer3? "answer3" : "hideButton"} onClick = {props.onClickAnswer}>{props.answer3}</button>
                 <button className={props.answer4? "answer4" : "hideButton"} onClick = {props.onClickAnswer}>{props.answer4}</button>
                 <button className="generateButton" onClick={props.onAddQuestions}>Generate question</button>
