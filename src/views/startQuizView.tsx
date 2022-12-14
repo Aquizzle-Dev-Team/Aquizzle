1fe08ddaffe76e5a308dcf38a518897a0cb436b8
zubair-y <muhammad.yousafzai@hotmail.com>
2022-12-14 18:07:31 +0100

    getting started with react spring

--- src/views/startQuizView.tsx
+++ src/views/startQuizView.tsx
@@ -1,12 +1,22 @@
 import logo from '../images/aquizzleLogo.png';
 import '../css/startQuiz.css';
+import { animated, useSpring } from '@react-spring/web';
 
 function StartQuizView(props: any){
+    const fade = useSpring({
+        from: {
+            opacity: 0
+        }, 
+        to: {
+            opacity: 1
+        }
+    })
+
     return(
-        <div className = "startQuizDiv">
+        <animated.div style={fade} className = "startQuizDiv">
             <img src={logo} alt="Logo"/>
-            <button onClick = {() => {window.location.hash="#quizselector";}} >Start a game</button>
-        </div>
+            <animated.button onClick = {() => {window.location.hash="#quizselector";}} >Start a game</animated.button>
+        </animated.div>
     )
 }
 
